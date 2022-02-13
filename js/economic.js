bfbbde1a5280d519704365756dfa92657c6024a4
Crocodiel <mk-spbkiselev@yandex.ru>
2022-02-13 21:23:38 +0300

    13.02

--- js/economic.js
+++ js/economic.js
@@ -69,3 +69,53 @@ ctx2 = document.getElementById("pie_expenses").getContext('2d')
 
 const pieIncome = new Chart(ctx,configPieIncome)
 const pieExpenses = new Chart(ctx2,configPieExpenses)
+
+// Здания
+class Building{
+	constructor(title, img, li_data,group){
+		this.title = title
+		this.img = img
+		this.li_data = li_data
+		this.group = group
+	}
+	createEl(){
+		let buildings = document.querySelector(".buildings")
+		let building__info = document.createElement("div")
+		building__info.setAttribute("value",this.group)
+		let building_title = document.createElement("span")
+		let building__text = document.createElement("ul")
+		let building_img = document.createElement("div")
+		building_img.className = "building_img"
+		building_img.innerHTML = `<img src="img/build/${this.img}.png">`
+		building__text.className = "building__text"
+		building_title.className = "building_title"
+		building__info.className = "building__info"
+
+		this.text_li = ["Производит: ","% от прибыли: ", "Доход: ","Всего зданий: ","Улучшение: "]
+		for(let i=0;i<5;i++){
+			let li = document.createElement("li")
+			if(i == 2){
+				li.innerHTML = `${this.text_li[i]}<span id="stonks" class="building_maintext">${this.li_data[i]}</span>`
+			}else if(i == 4){
+				li.innerHTML = `${this.text_li[i]}<span id="cost_upgrade" class="building_maintext">${this.li_data[i]}</span>`
+			}else{
+				li.innerHTML = `${this.text_li[i]}<span class="building_maintext">${this.li_data[i]}</span>`
+			}
+			building__text.append(li)
+		}
+		building_title.innerText = this.title
+		building__info.append(building_title)
+		building__info.append(building__text)
+		building__info.append(building_img)
+		buildings.append(building__info)
+	}
+}
+li_data = ["Батончики","20%","10$","2/2","1000$"]
+let a = new Building("А.П Мигунов","ap_migunov",li_data,"food")
+let b = new Building("А.П Мигунов","ap_migunov",li_data,"solders")
+let c = new Building("А.П Мигунов","ap_migunov",li_data,"solders")
+a.createEl()
+b.createEl()
+c.createEl()
+
+const building_list = []
